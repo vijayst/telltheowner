7aa6dc3dabe22bac506f404c7df0e5ab1fbbacc5
Vijay Thirugnanam <vijayst@gmail.com>
2026-05-24 19:16:35 -0400

    remove blog link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -222,7 +222,6 @@ export default function Home() {
               <h4 className="font-bold mb-4">Company</h4>
               <ul className="space-y-2 text-gray-400">
                 <li><a href="/about" className="hover:text-white transition">About</a></li>
-                <li><a href="#" className="hover:text-white transition">Blog</a></li>
                 <li><a href="#" className="hover:text-white transition">Contact</a></li>
               </ul>
             </div>
