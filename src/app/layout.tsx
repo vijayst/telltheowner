5ccfce8d4a6afe3334a967e5d96b17f187a7ef68
Vijay Thirugnanam <vijayst@gmail.com>
2026-05-30 12:09:57 -0400

    add analytis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 
@@ -36,6 +37,18 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json"></link>
       </head>
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EGRF1EZ2YE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EGRF1EZ2YE');
+          `}
+        </Script>
         <ServiceWorkerRegister />
         {children}
       </body>
